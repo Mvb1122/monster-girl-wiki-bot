441bedeb9c68174253a4f82abc07279a6939f4b5
Mb1122 <micahbushman.school@gmail.com>
2021-03-04 21:17:23 -0700

    Added m!Girl command
    yeah

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ client.on('message', message => {
 			color: 000000,
 			title: ("Commands:"),
 			fields: [
-			  { name: "Input", value: "m!random", inline: true},
-			  { name: "Result", value: "Sends a random Monster Girl from the Monster Girl Wiki.", inline: true}
+			  { name: "Input", value: "m!random\nm!girl XX", inline: true},
+			  { name: "Result", value: "Sends a random Monster Girl from the Monster Girl Wiki.\nreplace XX with the name of a gal and it'll send the cooresponding embed.", inline: true}
 			]}			
 		})
+    }
+    if (message.content.startsWith(`${config.prefix}girl`)) {
+        let girl = getGirlFromName(message.content);
+        message.channel.send(girl)
     }
 })
 
@@ -40,6 +44,33 @@ const getGirl = () => {
     const girlEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle(randomGirl)
+        .setURL(girlFile.url)
+        .setAuthor("Nokris, Supplicant to Savathûn", 'https://ihaveawebsite.tk/cdn/logo.png', 'https://ihaveawebsite.tk')
+        .setThumbnail(thumbnailURL)
+        .addFields(
+            { name: 'Description', value: girlFile.reason },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'rating', value: girlFile.rating, inline: true },
+            { name: 'MGE URL', value: girlFile.url, inline: true },
+        )
+        /*
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        */
+        .setTimestamp()
+        .setFooter('Made by MVB', 'https://ihaveawebsite.tk/cdn/logo.png');
+    console.log("Somebody used the bot.")
+    return girlEmbed
+}
+
+const getGirlFromName = (name) => {
+    name = name.slice(config.prefix.length + "girl".length + 1)
+    const girlFile = require(`./entries/${name}.json`);
+    const thumbnailURL = girlFile.thumbURL;
+
+    const girlEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(girlFile.id)
         .setURL(girlFile.url)
         .setAuthor("Nokris, Supplicant to Savathûn", 'https://ihaveawebsite.tk/cdn/logo.png', 'https://ihaveawebsite.tk')
         .setThumbnail(thumbnailURL)
